2eb3aba8a5420615b47486727491233ad0669574
IsmHernandez11 <ismasnayhernandez2010@gmail.com>
2026-04-20 20:55:43 -0500

    parte 4 y 5 listas

--- src/com/krakedev/sistemafinanciero/Cuenta.java
+++ src/com/krakedev/sistemafinanciero/Cuenta.java
@@ -1,45 +1,64 @@
 package com.krakedev.sistemafinanciero;
 
+
 public class Cuenta {
 
-	private String id;
-	private double saldoActual;
-	private String tipo;
+    private String id;
+    private double saldoActual;
+    private String tipo; 
+    private Cliente propietario; 
 
-	// Constructor
-	public Cuenta(String id) {
-		this.id = id;
-		this.saldoActual = 0;
-		this.tipo = "A";
-	}
+    // Constructor
+    public Cuenta(String id) {
+        this.id = id;
+        this.saldoActual = 0;
+        this.tipo = "A";
+    }
 
-	public String getId() {
-		return id;
-	}
+    // Getters
+    public String getId() {
+        return id;
+    }
 
-	public double getSaldoActual() {
-		return saldoActual;
-	}
+    public double getSaldoActual() {
+        return saldoActual;
+    }
 
-	public String getTipo() {
-		return tipo;
-	}
+    public String getTipo() {
+        return tipo;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public Cliente getPropietario() {
+        return propietario;
+    }
 
-	public void setSaldoActual(double saldoActual) {
-		this.saldoActual = saldoActual;
-	}
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
+    public void setSaldoActual(double saldoActual) {
+        this.saldoActual = saldoActual;
+    }
 
-	public void imprimir() {
-		System.out.println("ID: " + id);
-		System.out.println("Saldo Actual: " + saldoActual);
-		System.out.println("Tipo: " + tipo);
-	}
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setPropietario(Cliente propietario) {
+        this.propietario = propietario;
+    }
+
+    // Método imprimir
+    public void imprimir() {
+        System.out.println("ID: " + id);
+        System.out.println("Saldo Actual: " + saldoActual);
+        System.out.println("Tipo: " + tipo);
+
+        if (propietario != null) {
+            System.out.println("Cédula cliente: " + propietario.getCedula());
+        } else {
+            System.out.println("Sin propietario asignado");
+        }
+    }
 }
